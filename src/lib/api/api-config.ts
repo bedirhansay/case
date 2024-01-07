5584fa0051e9649bba99740edebe42eebd301b92
bedirhansay <bedirhan.sayy@gmail.com>
2024-01-07 18:46:51 +0300

    case

--- src/lib/api/api-config.ts
+++ src/lib/api/api-config.ts
@@ -1,11 +1,19 @@
-const { NEXT_PUBLIC_API_URL } = process.env;
+const { NEXT_PUBLIC_API_URL, NODE_ENV } = process.env;
+
+const getApiUrl = (): string => {
+  if (NODE_ENV === "development") {
+    return NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+  } else {
+    return NEXT_PUBLIC_API_URL || "https://production-api-url.com";
+  }
+};
+
+export const DEFAULT_API_CONFIG: ApiConfig = {
+  url: getApiUrl(),
+  timeout: 10000,
+};
 
 export interface ApiConfig {
   url: string | undefined;
   timeout: number;
 }
-
-export const DEFAULT_API_CONFIG: ApiConfig = {
-  url: NEXT_PUBLIC_API_URL,
-  timeout: 10000,
-};
