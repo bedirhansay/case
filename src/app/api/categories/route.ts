8630ef37a2945933c13683694a342e3161651f43
bedirhansay <bedirhan.sayy@gmail.com>
2024-01-07 18:50:20 +0300

    case

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 const { NEXT_PUBLIC_MOCK_API_URL } = process.env;
-
+export const dynamic = "force-dynamic";
 export async function GET(request: NextRequest, response: NextResponse) {
   const { data } = await axios(NEXT_PUBLIC_MOCK_API_URL as string);
   const brands = [...new Set(data.map((item: any) => item.brand))];
